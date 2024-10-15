0ca662741f198c63f99ca86a988f806743907b81
Jean Fruleux <jean.fruleux@gmail.com>
2024-10-15 09:14:47 +0200

    DB + PDO request

--- TP4/exo2/users.php
+++ TP4/exo2/users.php
@@ -1,4 +1,5 @@
 <?php
+echo '<link rel="stylesheet" type="text/css" href="style.css" />';
  require_once('config.php');
  $connectionString = "mysql:host=". _MYSQL_HOST;
  if(defined('_MYSQL_PORT'))
@@ -13,11 +14,26 @@
  catch (PDOException $erreur) {
  echo 'Erreur : '.$erreur->getMessage();
  }
- $request = $pdo->prepare("select * from users");
- echo $request;
+ 
+ //$request = $pdo->prepare("select * from users");
+ $request = $pdo->query("SELECT * from users");
+ $data = $request->fetch(PDO::FETCH_OBJ);
+ echo "<h1>Users</h1>";
+ echo "<hr align=left width=230px/><table>
+            <tr>
+               <th scope='col'>Id</th>
+               <th scope='col'>Nom</th>
+               <th scope='col'>email</th>
+            </tr>";
+ while(!empty($data)){  
+   echo '<tr><td>'.$data->id.'</td><td> '.$data->name.'</td><td> '.$data->email.'</td></tr>';
+   $data = $request->fetch(PDO::FETCH_OBJ);
+ }
+ echo "</tbody>
+ </table><hr align=left width=230px/>";
  // add your code here
  // retrieve data from database using fetch(PDO::FETCH_OBJ) and
  // display them in HTML array
  /*** close the database connection ***/
- $pdo = null;
+ //$pdo = null;
  ?>
